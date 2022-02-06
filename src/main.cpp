dcfdc1b9f559216e65846670fc0a3697c13da305
Arian Ghoddousi <arian.ghoddousi@gmail.com>
2022-02-06 23:02:08 +0100

    Removed unused includes

--- src/main.cpp
+++ src/main.cpp
@@ -5,16 +5,21 @@
 // Date: 06 Feb 2022
 //
 
-#include <iostream>
 #include <sstream>
 #include <string>
-#include <vector>
 
 #include "adsbcore.hpp"
 
 int main(int argc, char** argv)
-{
-    std::string data = "8d3b756b251811b4c39e6082017d";
+{   
+    std::string data = "";
+    if(argc < 2)
+    {
+        data = "8d3b756b251811b4c39e6082017d";
+    }else
+    {
+        data = argv[1];
+    }
 
     aviware::adsb::AdsbCore core(data.c_str());
 
